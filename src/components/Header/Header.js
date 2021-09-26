2552d54f553ac243512380b747d325dcd805e042
md-abdul-ahammed <shagarmd2082@gmail.com>
2021-09-26 22:43:41 +0600

    again-done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='primary-bg-color text-center p-3'>
             <h1>Make Tourist Team</h1>
             <p>I have plan for travelling all over the world. So need some partner.</p>
-            <h1>Minimum Budget For Next Trip: <span className='fw-bold text-warning'><i className="fas fa-fighter-jet me-2 ms-3"></i>$1500</span></h1>
+            <h1>Total Budget For Next Trip: <span className='fw-bold text-warning'><i className="fas fa-fighter-jet me-2 ms-3"></i>$1500</span></h1>
         </div>
     );
 };
